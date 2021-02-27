58a46c4a642a510f95dc7668a259fed6c15dc535
kedama <hades.netherworld@gmail.com>
2021-02-28 02:24:40 +0900

    run spotless

--- app/src/main/java/com/example/androiddevchallenge/ui/page/list/Animals.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/page/list/Animals.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.page.list
 
 import androidx.compose.foundation.Image
@@ -62,7 +77,8 @@ fun Animals(selectAnimal: (Long) -> Unit, animalsViewModel: AnimalsViewModel = v
         selectAnimal = selectAnimal,
         selectFilter = {
             animalsViewModel.selectFilter(it)
-        })
+        }
+    )
 }
 
 @Composable
@@ -104,15 +120,18 @@ private fun FilterMenu(
         IconButton(
             modifier = Modifier
                 .size(40.dp),
-            onClick = { expanded = true }) {
+            onClick = { expanded = true }
+        ) {
             Icon(imageVector = Icons.Default.Filter, null)
         }
         DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
             filters.forEach { filter ->
-                DropdownMenuItem(onClick = {
-                    selectFilter(filter)
-                    expanded = false
-                }) {
+                DropdownMenuItem(
+                    onClick = {
+                        selectFilter(filter)
+                        expanded = false
+                    }
+                ) {
                     if (filter == selected) {
                         Text(text = filter.name)
                     } else {
@@ -190,11 +209,13 @@ private fun AnimalItem(
                     Spacer(
                         modifier = Modifier
                             .requiredSize(60.dp)
-                            .clip(GenericShape { size, _ ->
-                                lineTo(size.width / 3, 0F)
-                                lineTo(0F, size.height / 3)
-                                lineTo(0F, 0F)
-                            })
+                            .clip(
+                                GenericShape { size, _ ->
+                                    lineTo(size.width / 3, 0F)
+                                    lineTo(0F, size.height / 3)
+                                    lineTo(0F, 0F)
+                                }
+                            )
                             .background(color = pink)
                             .align(Alignment.TopStart)
                     )
@@ -237,7 +258,6 @@ private fun AnimalItem(
                     }
             )
         }
-
     }
 }
 
--- app/src/main/java/com/example/androiddevchallenge/vm/AnimalsViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/vm/AnimalsViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.vm
 
 import androidx.lifecycle.LiveData
@@ -56,9 +71,11 @@ class AnimalsViewModel : ViewModel() {
             animalRepository.toggleFavorite(id).collect {
                 val animal = it ?: return@collect
                 val list = _animalList.value ?: return@collect
-                _animalList.postValue(list.map { current ->
-                    if (current.id == animal.id) animal else current
-                })
+                _animalList.postValue(
+                    list.map { current ->
+                        if (current.id == animal.id) animal else current
+                    }
+                )
                 println(animal)
                 println(_animalList.value?.firstOrNull())
                 _detailAnimal.postValue(animal)
